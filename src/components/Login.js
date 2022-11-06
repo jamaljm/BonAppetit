9c95214994a14acfbc70867dd6e1c759b7efb13f
jam-dev <jamalvga2002@gmail.com>
2022-11-06 10:38:35 +0530

    jnf

--- src/components/Login.js
+++ src/components/Login.js
@@ -76,37 +76,37 @@ return (
  <div className=' flex  items-center justify-center '>
 
   
-  <div className="p-4 w-full max-w-sm bg-white rounded-lg border border-gray-200 shadow-md sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
-    <form className="space-y-6" onSubmit={handleSubmit}> 
-        <h5 className="text-xl font-medium text-gray-900 dark:text-white">Sign in to our platform</h5>
+  <div className="">
+    <form className="" onSubmit={handleSubmit}> 
+        <h5 className="">Sign in to our platform</h5>
         <div>
-            <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
+            <label  className="">Your email</label>
             <input type="email"
     name="email"
     placeholder="First Name"
     value={state.username}
-    onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required/>
+    onChange={handleChange} className="" required/>
         </div>
         <div>
-            <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your password</label>
+            <label for="password" className="">Your password</label>
             <input  type="password"
     name="password"
     placeholder="Password"
     value={state.password}
-    onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required/>
+    onChange={handleChange} className="" required/>
         </div>
-        <div className="flex items-start">
-            <div className="flex items-start">
-                <div className="flex items-center h-5">
-                    <input id="remember" type="checkbox" value="" className="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" />
+        <div className="">
+            <div className="">
+                <div className="">
+                    <input id="remember" type="checkbox" value="" className="" />
                 </div>
-                <label for="remember" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
+                <label for="remember" className="">Remember me</label>
             </div>
-            <a href="#" className="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
+            <a href="#" className="">Lost Password?</a>
         </div>
-        <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
-        <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
-            Not registered? <a href="#" className="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
+        <button type="submit" className="">Login to your account</button>
+        <div className="">
+            Not registered? <a href="#" className="">Create account</a>
         </div>
     </form>
 </div>
@@ -115,37 +115,37 @@ return (
 </div>
 
 
- <div className="p-4 w-full max-w-sm bg-white rounded-lg border border-gray-200 shadow-md sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
-    <form className="space-y-6" onSubmit={handleSubmitorg}> 
-        <h5 className="text-xl font-medium text-gray-900 dark:text-white">Sign in to our platform</h5>
+ <div className="">
+    <form className="" onSubmit={handleSubmitorg}> 
+        <h5 className="">Sign in to our platform</h5>
         <div>
-            <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
+            <label  className="">Your email</label>
             <input type="email"
     name="email"
     placeholder="First Name"
     value={state.username}
-    onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required/>
+    onChange={handleChange} className="" required/>
         </div>
         <div>
-            <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your password</label>
+            <label for="password" className="">Your password</label>
             <input  type="password"
     name="password"
     placeholder="Password"
     value={state.password}
-    onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required/>
+    onChange={handleChange} className="" required/>
         </div>
-        <div className="flex items-start">
-            <div className="flex items-start">
-                <div className="flex items-center h-5">
-                    <input id="remember" type="checkbox" value="" className="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" />
+        <div className="">
+            <div className="">
+                <div className="">
+                    <input id="remember" type="checkbox" value="" className="" />
                 </div>
-                <label for="remember" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
+                <label for="remember" className="">Remember me</label>
             </div>
-            <a href="#" className="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
+            <a href="#" className="">Lost Password?</a>
         </div>
-        <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
-        <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
-            Not registered? <a href="#" className="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
+        <button type="submit" className="">Login to your account</button>
+        <div className="">
+            Not registered? <a href="#" className="">Create account</a>
         </div>
     </form>
 </div>
--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { Navigate, useNavigate } from "react-router-dom";
+import "./signin.css";
+
 
 function SignupPage() {
   const navigate = useNavigate();
@@ -71,161 +73,106 @@ function SignupPage() {
   //create a drop down with 2 options
 
   return (
-    <div className=" flex  items-center justify-center ">
-      <div className="p-4 w-full max-w-sm bg-white rounded-lg border border-gray-200 shadow-md sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
-        <form className="space-y-6" onSubmit={handleSubmit1}>
-          <h5 className="text-xl font-medium text-gray-900 dark:text-white">
-            Sign in to our platform
+    <div className="signupcontainer">
+      <div className="signupcard">
+        <form className="signupform" onSubmit={handleSubmit1}>
+          <h5 className="signupheading">
+            Sign up for Volunteers
           </h5>
           <div>
-            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-              Your email
+            <label className="">
             </label>
             <input
               type="email"
               name="email"
-              placeholder="First Name"
+              placeholder="abc@gmail.com"
               value={state.username}
               onChange={handleChange}
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+              className=""
               required
             />
           </div>
           <div>
-            <label
-              for="password"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-            >
-              Your password
-            </label>
+           
             <input
               type="password"
               name="password"
               placeholder="Password"
               value={state.password}
               onChange={handleChange}
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+              className=""
               required
             />
           </div>
-          <div className="flex items-start">
-            <div className="flex items-start">
-              <div className="flex items-center h-5">
-                <input
-                  id="remember"
-                  type="checkbox"
-                  value=""
-                  className="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
-                />
-              </div>
-              <label
-                for="remember"
-                className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-              >
-                Remember me
-              </label>
-            </div>
-            <a
-              href="#"
-              className="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500"
-            >
-              Lost Password?
-            </a>
-          </div>
+          
           <button
             type="submit"
-            className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className=""
           >
-            Login to your account
+            Signup
           </button>
-          <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
-            Not registered?{" "}
+          <div className="alreadyaccou">
+            Already have account?{" "}
             <a
               href="#"
-              className="text-blue-700 hover:underline dark:text-blue-500"
+              className=""
             >
-              Create account
+              Login
             </a>
           </div>
-        </form>
-      </div>
-      <div>
+             <div>
         <h1>{login}</h1>
       </div>
+        </form>
+      
+      </div>
+     
 
-      <div className="p-4 w-full max-w-sm bg-white rounded-lg border border-gray-200 shadow-md sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
-        <form className="space-y-6" onSubmit={handleSubmitorg}>
-          <h5 className="text-xl font-medium text-gray-900 dark:text-white">
-            Sign in to our platform
+      <div className="signupcard">
+        <form className="signupform" onSubmit={handleSubmitorg}>
+          <h5 className="signupheading">
+            Sign Up For Contributers
           </h5>
           <div>
-            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-              Your email
-            </label>
+           
             <input
               type="email"
               name="email"
-              placeholder="First Name"
+              placeholder="abc@gmail.com"
               value={state.username}
               onChange={handleChange}
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+              className=""
               required
+              def
+              
             />
           </div>
           <div>
-            <label
-              for="password"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-            >
-              Your password
-            </label>
+            
             <input
               type="password"
               name="password"
               placeholder="Password"
               value={state.password}
               onChange={handleChange}
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+              className=""
               required
             />
           </div>
-          <div className="flex items-start">
-            <div className="flex items-start">
-              <div className="flex items-center h-5">
-                <input
-                  id="remember"
-                  type="checkbox"
-                  value=""
-                  className="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
-                />
-              </div>
-              <label
-                for="remember"
-                className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-              >
-                Remember me
-              </label>
-            </div>
-            <a
-              href="#"
-              className="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500"
-            >
-              Lost Password?
-            </a>
-          </div>
+         
           <button
             type="submit"
-            className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className=""
           >
-            Login to your account
+            Signup
           </button>
-          <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
-            Not registered?{" "}
+          <div className="">
+            Already have account?{" "}
             <a
               href="#"
-              className="text-blue-700 hover:underline dark:text-blue-500"
+              className=""
             >
-              Create account
+              Login
             </a>
           </div>
         </form>
